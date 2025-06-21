9c92935f1e679577714205c185ca68aebf5eaf23
Darsh Parikh <iamdarsh2424@gmail.com>
2025-06-21 19:32:13 +0530

    changes cause deployment issues

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
    origin: [
     "http://localhost:5173",
-    "htps://6856b90ce037d5c21bff95a8--twitterclone-darsh.netlify.app"
+    "https://6856b90ce037d5c21bff95a8--twitterclone-darsh.netlify.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type"],
